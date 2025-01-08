17a565a97a92191a417286fc65ae9959af3ca05a
natetongwong@gmail.com <natetongwong@gmail.com>
2025-01-08 10:35:07 +0000

    Fix typo in column name from `lasst_purchase_date` to `last_purchase_date` in Test pipeline rules.

--- pipelines/Test/code/test/functions/Rules.py
+++ pipelines/Test/code/test/functions/Rules.py
@@ -8,12 +8,12 @@ def PromoCodeRule(
         spending: Column=lambda: col("spending"), 
         high_value_flag: Column=lambda: col("high_value_flag"), 
         is_recent_order: Column=lambda: col("is_recent_order"), 
-        lasst_purchase_date: Column=lambda: col("lasst_purchase_date")
+        last_purchase_date: Column=lambda: col("last_purchase_date")
 ):
     return when(((spending > lit(700)) & (high_value_flag == lit(1))), lit("25% Discount"))\
         .when((spending > lit(500)), lit("15% Discount"))\
         .when(((spending > lit(500)) & (is_recent_order == lit(1))), lit("Buy one get one free"))\
         .when((spending > lit(300)), lit("10% Discount"))\
-        .when(((lasst_purchase_date >= lit(20241201)) & (col("order_date") <= lit(20250101))), lit("Free two-day shipping"))\
+        .when(((last_purchase_date >= lit(20241201)) & (col("order_date") <= lit(20250101))), lit("Free two-day shipping"))\
         .otherwise(lit("5% Discount"))\
         .alias("promo_offer")
